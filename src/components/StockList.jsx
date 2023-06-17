51d679a0fec6f852723b1bc0f437ce8fb08033b5
AndrewChacon <andrew.b.chacon@gmail.com>
2023-06-17 14:09:29 -0400

    fetch data from a stock

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import finnHub from '../apis/finnHub';
 
 const StockList = () => {
+	const [stock, setStock] = useState();
+
+	const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZ']);
+
+	useEffect(() => {
+		let isMounted = true;
+		const fetchData = async () => {
+			try {
+				const response = await finnHub.get('/quote?', {
+					symbol: 'MSFT',
+				});
+				console.log(response);
+				if (isMounted) {
+					setStock(response.data);
+				}
+			} catch (error) {}
+		};
+		fetchData();
+		return () => (isMounted = false);
+	}, []);
+
 	return <div>StockList</div>;
 };
 
